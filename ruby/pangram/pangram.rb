7558dc8491a7e170fda79aecea040a002797095a
woodall <dave@woodalls.me>
2016-05-22 19:34:25 -0400

    suggestions by @bcc32

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,42 +1,52 @@
 module Pangram
   VERSION = 1
-  def self.is_pangram?(string, engine=Engine::StringAnalyzer)
-    engine.new(string, analyze: 'pangram').run
+  def self.is_pangram?(string, engine=StringAnalyzer)
+    engine.new(string, task: :pangram).run_task
   end
 end
 
-module Engine
-  class StringAnalyzer
+class PangramAnalyzer
+  attr_reader :string
 
-    attr_reader :string, :action
+  def initialize(string)
+    @string = string
+  end
 
-    def initialize(string, action = {} )
-      @string = string
-      @action = action[:analyze]
+  def run
+    is_pangram? normalize_string
+  end
+
+private
+
+  def normalize_string
+    string.chars.sort.uniq.join
+  end
+
+  def is_pangram?(string)
+    alf = ('a'..'z').to_a
+
+    string.split('').each do |letter|
+      alf.delete(letter.downcase)
     end
 
-    def run
-      self.send(action)
-    end
+    alf.length == 0 # nice! Thanks, @rbndickson
+  end
+end
 
-    def pangram
-      judge alphabetize_string!
-    end
+class StringAnalyzer
+  attr_reader :action
 
-  private
+  ANALYZERS = {
+    pangram: PangramAnalyzer,
+    # pretend: PretendAnalyzer
+  }
 
-    def alphabetize_string!
-      string.chars.sort.uniq.join
-    end
+  def initialize(string, selected={} )
+    analyzer = ANALYZERS[selected[:task]]
+    @action = analyzer.new(string)
+  end
 
-    def judge(string)
-      alf = ('a'..'z').to_a
-
-      string.split('').each do |letter|
-        alf.delete(letter.downcase)
-      end
-
-      alf.length == 0 # nice! Thanks, @rbndickson
-    end
+  def run_task
+    action.run
   end
 end
